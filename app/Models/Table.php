3fbd6eaf6920f08d98cc86c202b453e9237af574
Pieter Vardi <pietervardi@gmail.com>
2025-03-24 00:33:00 +0700

    feat: create table chair api

---
+++ app/Models/Table.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Table extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tables';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'outlet_id',
+        'number',
+        'is_available',
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+        'outlet_id' => 'string',
+        'is_available' => 'boolean',
+    ];
+
+    public function outlet()
+    {
+        return $this->belongsTo(Outlet::class, 'outlet_id');
+    }
+}
---
+++ database/migrations/2025_03_16_114558_create_tables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            if(config('database.default') == 'pgsql')
+                $table->uuid('id')->primary()->default(DB::raw('gen_random_uuid()'));
+            else if(config('database.default') == 'mysql')
+                $table->uuid('id')->primary()->default(DB::raw('uuid()'));
+            $table->uuid('outlet_id');
+            $table->string('number');
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
